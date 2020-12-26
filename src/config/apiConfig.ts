109808a6569a59591432c2e544098f251fe04be5
Rishad Bharucha <rb2102@gmail.com>
2020-12-26 14:37:13 -0600

    set up api redirect

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -11,16 +11,10 @@ interface ApiConfiguration {
   };
 }
 
-export const CORS_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://0.0.0.0:8080/'
-    : 'https://cors-anywhere.herokuapp.com/';
 export const CURRENCY_API = 'https://api.exchangerate-api.com/v4/latest/';
 
-const HOST_URL = `${CORS_URL}https://sahmed93846.api-us1.com/api/`;
-
 export const apiConfig: ApiConfiguration = {
-  host: HOST_URL,
+  host: '/api/',
   version: '3',
   contacts: '/contacts/',
   contactTags: '/contactTags/',
